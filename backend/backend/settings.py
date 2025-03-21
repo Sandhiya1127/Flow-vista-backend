377d007d203d02df544d6889091c0df2bcb5c1d7
sandhiya <sandhiya.s@prowesstics.com>
2025-03-21 16:24:37 +0530

    space

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,11 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
     # 'django_tenants',
     # 'django_multitenant',
+    'corsheaders',  #
     'rest_framework',
     'rest_framework_simplejwt',
     'django.contrib.admin',
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +58,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React frontend URL
+]
+
+CORS_ALLOW_ALL_ORIGINS = True #only for development
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -102,6 +111,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 
 
 DATABASES = {
